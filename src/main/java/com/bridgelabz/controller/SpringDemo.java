1938dc6cb9c493bb5ede6c6c4554b93424a7c698
SAHIL0455 <sahilpawar1004@gmail.com>
2022-11-06 22:15:15 +0530

    ADDED: Using get request method to create the API

--- src/main/java/com/bridgelabz/controller/SpringDemo.java
+++ src/main/java/com/bridgelabz/controller/SpringDemo.java
@@ -4,5 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SpringDemo {
-
+    @RequestMapping(value = {"/body"})
+      public String sayHello(){
+       return "Hello from BridgeLabz";
+       }
 }
