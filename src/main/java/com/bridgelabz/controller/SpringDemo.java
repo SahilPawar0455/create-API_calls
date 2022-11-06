855e6b7baef92be9fc5f054c3828d206830317aa
SAHIL0455 <sahilpawar1004@gmail.com>
2022-11-06 22:26:34 +0530

    ADDED: Use get request method and pass the parameter

--- src/main/java/com/bridgelabz/controller/SpringDemo.java
+++ src/main/java/com/bridgelabz/controller/SpringDemo.java
@@ -5,7 +5,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SpringDemo {
     @RequestMapping(value = {"/body"})
-      public String sayHello(){
+    public String sayHello(){
        return "Hello from BridgeLabz";
-       }
+    }
+
+    @RequestMapping(value = {"/body1"})
+    public String sayHello1(@RequestParam(value = "name") String name){
+        return "Hello "+ name+" From Bridgelabz";
+    }
+
 }
